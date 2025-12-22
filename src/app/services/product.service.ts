c6e5e65e6a6fb5ace75865bb23205acc364a59b9
JuanZu77 <juan_zubiri@hotmail.com>
2025-12-21 21:33:19 -0300

    implementation of the home component

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl: string = 'http://localhost:8080/api/v1/admin/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.apiUrl);
+  }
+
+  createProduct(formData: FormData): Observable<any> {
+  return this.httpClient.post<Product>(this.apiUrl, formData);
+  }
+
+  deleteProduct(id: number):Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/${id}`);
+  }
+
+  getProductById(id: number):Observable<Product> {
+    return this.httpClient.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
+  /*updateProduct(product: Product) {
+    return this.httpClient.put<Product>(`${this.apiUrl}/${product.id}`, product);
+  }*/
+
+}
+
